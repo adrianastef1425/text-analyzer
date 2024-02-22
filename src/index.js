0fa2232b400a01ba2bf398aa70d8e78e82881f78
Adri <adrianastef1425@gmail.com>
2024-02-22 11:48:28 -0500

    mi primer cambio

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+var datoform = document.getElementById("datorecibido");
+var input = document.getElementsByTagName("dato");
+const caracteres = input.length;
+datoform.textContent=" Caracteresl: "+ caracteres;
+
