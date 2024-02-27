3c58872d2d89d326cf9b337e5b68fa641a2fd8d4
Adri <adrianastef1425@gmail.com>
2024-02-26 23:15:41 -0500

    quinto cambio

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,15 +1,32 @@
+function numeropalabras(text){
+  const datox = text.value.trim().split(" ").length;
+  return datox;
+}
+
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    text = document.querySelector("textarea[name='user-input']");
-    const datolista2 = document.querySelectorAll(".datolista2");
-    const datox = text.value;
-    const datoy = datox.trim().split(" ");
+    //text = document.querySelector("textarea[name='user-input']");
+    //const datolista2 = document.querySelectorAll(".datolista2");
+    console.log(text);
+    /*
+    const datox = text;
+    const datoz = datox.value;
+    const datoy = datoz.trim().split(" ");
     //const datoz = datoy.split(" ");
     datolista2.forEach(function(dato){
       dato.textContent = datoy.length;
     })
-  
+    */
+    const datox = text.value.trim().split(" ").length;
+
+    console.log(datox);
+    /*
+    clase2.forEach(function(dato){
+      dato.textContent = datox.length;
+     })
+    */
+   return datox;
   },
 
   getCharacterCount: (text) => {
@@ -22,43 +39,102 @@ const analyzer = {
     datoLabel.textContent = conteocaracteres;
     */
     //////////////////////////////////////////////
-    text = document.querySelector("textarea[name='user-input']");
-    //console.log(text);
-    const datolista1 = document.querySelectorAll(".datolista1");
-    //console.log(datolista1);
-    datolista1.forEach(function(dato){
-      dato.textContent = text.value.length;
-      //console.log(datolista1);
-    })
-
+    const datox = text.value.length;
+    return datox
   },
 
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const signos = ['!', '#', '$', '%', '&', '/', '(', ')', '¿', '¡','?', '{', '}', '[', ']', ' '];
+    let newtext = text.value.split("");
+    let datox = text.value;
+    let out = [];
+    for(let i = 0; i < text.value.length; i++){
+      for(let j = 0; j < signos.length; j++){
+        if(newtext[i] == signos[j]){
+          let sign = newtext[i];
+          out = out.concat(sign);
+          datox = datox.replace(signos[j], "");
+        }
+      }
+    }
     
+    console.log(datox);
+    let cantidad = datox.length;
+    return cantidad;
     
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    text = document.querySelector("textarea[name='user-input']").value;
-      
-    let newtext = text.replace(" ", "");
-    console.log(newtext);
-      
-  
+    const numeropalabras2 = numeropalabras(text);
+    const datox = text.value.replace(/\s/g, "");
+    const datoy = (datox.length)/numeropalabras2;
+    //const newtext2 = newtext.split("");
+    //console.log(newtext);
+    //const datolista1 = document.querySelectorAll(".datolista6");
     
-    const datolista1 = document.querySelectorAll(".datolista6");
-    //console.log(datolista1);
+    //let cantidadpalabras = this.getWordCount();
+    //obtener la cantidad de palabras
+    /*
+    const text2 = document.querySelector("textarea[name='user-input']");
+    const datolista2 = document.querySelectorAll(".datolista6");
+    const datox = text2.value;
+    const datoy = datox.trim().split(" ");
+    datolista2.forEach(function(dato){
+      dato.textContent = datoy.length;
+    })
+    */
+
+    /*
     datolista1.forEach(function(dato){
-      dato.textContent = newtext.length;
+      dato.textContent = (newtext.length)///cantidadpalabras;
      
     })
+    */
+   return datoy;
+    
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  const numeros = ['0' , '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let newtext = text.value.trim().split("");
+  console.log(newtext);
+  let conteo = 0;
+  for(let i = 0; i < newtext.length; i++){
+    for(let j = 0; j < numeros.length; j++){
+      if(newtext[i] == numeros[j]){
+        conteo = conteo + 1;
+        console.log(conteo);
+      }
+    }
+  }
+  return conteo;
   },
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numeros = ['0' , '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let newtext = text.value.trim().split("");
+    console.log(newtext);
+    let conteo = 0;
+    let conteo2 = [];
+    let sumanumeros = 0;
+    for(let i = 0; i < newtext.length; i++){
+      for(let j = 0; j < numeros.length; j++){
+        if(newtext[i] == numeros[j]){
+            conteo = newtext[i];
+            conteo2 = conteo2.concat(parseInt(conteo));
+              console.log(conteo2);
+          
+        }
+      }
+    }
+    for(let i = 0; i < conteo2.length; i++){
+      sumanumeros = sumanumeros + conteo2[i];
+      console.log(sumanumeros);
+    }
+   
+    return sumanumeros;
   },
 
 };
--- src/index.js
+++ src/index.js
@@ -3,33 +3,29 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-// (HTML)
-/* 
-  <div>
-      <label>Caracteres: </label>
-      <label id="datolabel">0</label>
-  </div>
+const input = document.querySelector("textarea[name='user-input']");
+const datolista1 = document.querySelector(".datolista1");
+const datolista2 = document.querySelector(".datolista2");
+const datolista3 = document.querySelector(".datolista3");
+const datolista4 = document.querySelector(".datolista4");
+const datolista5 = document.querySelector(".datolista5");
+const datolista6 = document.querySelector(".datolista6");
 
-  <form>
-      <input type="text" name="datoform" id="datoform" />
-      <br />
-  </form>
-*/
+input.addEventListener("input", function() {
 
-/*
-const input1 = document.getElementById("datoform");
-input1.addEventListener("input", analyzer.getCharacterCount, false);
-input1.removeEventListener("input", analyzer.getWordCount);
-*/
-///////////////////////////////////////////////
-const input2 = document.querySelector("textarea[name='user-input']");
-input2.addEventListener("input", function() {
-    analyzer.getCharacterCount();
-    analyzer.getWordCount();
-    analyzer.getCharacterCountExcludingSpaces();
-    analyzer.getNumberCount();
-    analyzer.getNumberSum();
-    analyzer.getAverageWordLength();
+    const conteocaracteres = analyzer.getCharacterCount(input);
+    datolista1.textContent = conteocaracteres;
+    const conteopalabras = analyzer.getWordCount(input);
+    datolista2.textContent = conteopalabras;
+    const conteosinespacios = analyzer.getCharacterCountExcludingSpaces(input);
+    datolista3.textContent = conteosinespacios;
+    const conteonumeros = analyzer.getNumberCount(input);
+    datolista4.textContent = conteonumeros;
+    const sumanumeros = analyzer.getNumberSum(input);
+    datolista5.textContent = sumanumeros;
+    const conteomedia = analyzer.getAverageWordLength(input);
+    datolista6.textContent = conteomedia;
+
     // Código para manejar el evento de entrada en el textarea
 });
 
@@ -40,12 +36,24 @@ resetbutton.addEventListener("click", function(){
     let datotext = document.querySelector("textarea[name='user-input']");
     datotext.value = "";
 
-    let clase1 = document.querySelectorAll(".datolista1");
-    clase1.forEach(function(dato) {
-        dato.innerHTML = "0";
-    });
+    let clase1 = document.querySelector(".datolista1");
+    clase1.innerHTML = "0";
 
-    
+    let clase2 = document.querySelector(".datolista2");
+    clase2.innerHTML = "0";
+
+    let clase3 = document.querySelector(".datolista3");
+    clase3.innerHTML = "0";
+
+    let clase4 = document.querySelector(".datolista4");
+    clase4.innerHTML = "0";
+
+    let clase5 = document.querySelector(".datolista5");
+    clase5.innerHTML = "0";
+
+    let clase6 = document.querySelector(".datolista6");
+    clase6.innerHTML = "0";
+
 })
                 
 
